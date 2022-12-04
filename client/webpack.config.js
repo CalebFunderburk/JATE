475e2660f659444725be33d7a890d6bed03bfd3e
Caleb <calebfunderburk@icloud.com>
2022-12-04 15:53:00 -0500

    updated service worker code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -54,12 +54,12 @@ module.exports = () => {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
+              presets: ['@babel/preset-env'],
               plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime"
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime'
               ]
             }
           }
