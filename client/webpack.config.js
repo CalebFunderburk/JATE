473777bb9b00fb07ace48ce7b1cab801f8b5e368
Caleb <calebfunderburk@icloud.com>
2022-12-04 15:56:38 -0500

    rendering build in browser successfully

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,14 +17,14 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        favicon: './facison.ico',
+        favicon: './favicon.ico',
         title: 'Text Editor'
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'service-worker.js'
       }),
-      WebpackPwaManifest({
+      new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
         name: 'Just another text editor',
